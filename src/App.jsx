6c3f37d2d08a714ee0bcc2e6ad292b535aa50e11
vedant hundare <vedanthundare8@gmail.com>
2025-03-02 19:12:51 +0530

    Updated the CodeAI website

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,11 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Lenis from "@studio-freight/lenis";
 import Home from "./pages/Home";
 import About from "./pages/About";  
 import Teams from "./pages/Teams"
 import Footer from "./components/Footer"
 
 function App() {
-  useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.5,  
-      smooth: true,
-      lerp: 0.05,  
-    });
-
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-
-    return () => {
-      lenis.destroy(); 
-    };
-  }, []);
-
   return (
     <Router>
    <div id="scroll-section" className="min-h-screen bg-[#0a0118]">
